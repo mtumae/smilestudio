c6aabcb9e0f3641aac3f8a25e96cafac0be5cd70
kelvinwambua <shwiftykelvin@gmail.com>
2024-12-29 18:10:17 +0300

    Dynamic Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { Session } from "inspector/promises";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
+        <SessionProvider>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        </SessionProvider>  
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+"use client"
 import Image from "next/image";
-import { Arrow } from "@radix-ui/react-dropdown-menu";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,50 +9,83 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu"
 import Calendar from "./calendar/calendar";
+import { useSession, signOut } from "next-auth/react";
 
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { DoorOpen, LogOut, Settings, User2 } from "lucide-react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <div>
-    <div className="max-w-screen-xl bg-background flex flex-wrap items-center justify-between mx-auto p-4 text-black">
-      <Image
-        alt="Smile Studio logo"
-        src="/logo.png"
-        width={200}
-        height={200}
+      <div className="max-w-screen-xl bg-background flex flex-wrap items-center justify-between mx-auto p-4 text-black">
+        <Image
+          alt="Smile Studio logo"
+          src="/logo.png"
+          width={200}
+          height={200}
         />
-        <div className="flex items-center" >
-            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-black-500">
-                <li>
-                    <a href="#" className="text-gray-900 hover:text-ssblue p-4" aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Practice</a>
-                </li>
-                <li>
+        <div className="flex items-center">
+          <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-black-500">
+            <li>
+              <a href="#" className="text-gray-900 hover:text-ssblue p-4" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-900 hover:text-ssblue p-4">Practice</a>
+            </li>
+            <li>
+              <DropdownMenu>
+                <DropdownMenuTrigger>Services</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Services</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Hygiene</DropdownMenuItem>
+                  <DropdownMenuItem>Dentistry</DropdownMenuItem>
+                  <DropdownMenuItem>Orthodontics</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
+            <li>
+              <a href="#" className="text-gray-900 hover:text-ssblue p-4">Blog</a>
+            </li>
+            <li>
+              {session ? (
                 <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    Services
+                  <DropdownMenuTrigger className="flex items-center">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>{session.user.email?.charAt(0)}</AvatarFallback>
+                </Avatar>
+
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
-                    <DropdownMenuLabel>Services</DropdownMenuLabel>
+                    <DropdownMenuLabel>{session.user?.name ?? "Client"}</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>Hygiene</DropdownMenuItem>
-                    <DropdownMenuItem>Dentistry</DropdownMenuItem>
-                    <DropdownMenuItem>Orthodontics</DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <User2 size={16} className="mr-2" />
+                      Profile
+                      </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Settings size={16} className="mr-2" />
+                      Settings</DropdownMenuItem>
+                    <DropdownMenuItem className="text-red" onClick={() => signOut()}>
+                      <LogOut size={16} className="mr-2" />
+                      Logout
+                    </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
-                </li>
-                <li>
-                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Login</a>
-                </li>
-                <li>
-                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Blog</a>
-                </li>
-           </ul>
+              ) : (
+                <Link href="/api/auth/signin" className="text-gray-900 hover:text-ssblue p-4">
+                  Login
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
-    </div>
-    <Calendar></Calendar>
+      </div>
+      <Calendar></Calendar>
     </div>
   );
 }
