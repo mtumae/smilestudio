c9a7555ddf3230bb362a4ee5a9ba448070044b89
mtume <mtume2016@gmail.com>
2024-12-29 11:54:46 +0300

    Adding files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,55 @@
-import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
 
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
-
+export default function Home() {
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
-          </div>
-
-          {session?.user && <LatestPost />}
+    <div>
+    <div className="max-w-screen-xl bg-background flex flex-wrap items-center justify-between mx-auto p-4 text-black">
+      <Image
+        alt="Smile Studio logo"
+        src="/logo.png"
+        width={200}
+        height={200}
+        />
+        <div className="flex items-center" >
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-black-500">
+                <li>
+                    <a href="#" className="text-gray-900 hover:text-ssblue p-4" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Practice</a>
+                </li>
+                <li>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>Services</DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>Services</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Hygiene</DropdownMenuItem>
+                    <DropdownMenuItem>Dentistry</DropdownMenuItem>
+                    <DropdownMenuItem>Orthodontics</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                </li>
+                <li>
+                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Login</a>
+                </li>
+                <li>
+                    <a href="#" className="text-gray-900 hover:text-ssblue p-4">Blog</a>
+                </li>
+           </ul>
         </div>
-      </main>
-    </HydrateClient>
+    </div>
+    <footer>
+      
+    </footer>
+    </div>
   );
 }
