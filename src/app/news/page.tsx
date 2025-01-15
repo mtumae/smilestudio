0b0a0597feaa48f074d8e38f1dc7bd622c5d44f6
mtume <mtume2016@gmail.com>
2025-01-15 08:30:11 +0300

    Editing news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,11 @@
+"use client"
+
 import Home from "../page"
 import Footer from "../footer/page"
 import { Button } from "~/components/ui/button"
+import NavBar from "~/components/ui/navigation"
+import Autoplay from "embla-carousel-autoplay"
+
 import {
     Card,
     CardContent,
@@ -10,20 +15,63 @@ import {
     CardTitle,
   } from "~/components/ui/card"
 
+  import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "~/components/ui/carousel"
+  
 
 
+  const posts = {
+    title : "",
+    subtitle: "",
+    author: "Smile Studio Kenya",
+    body: "",
+  }
+
+  const Img1Style ={
+    backgroundImage: 'url(/homepageblur.jpg)',
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center',  
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'left',
+    padding: '20px',
+    margin: '20px',
+  };
+
+
+  const Img2Style ={
+    backgroundImage: 'url(/sign.jpg)',
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center',  
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'left',
+    padding: '20px',
+    margin: '20px',
+
+  }
 
 export default function News(){
     return(
         <div>
-        <div className="grid grid-auto-fit justify-center items-center gap-4 m-10">
+            <NavBar></NavBar>
+        
+            <div className="">
+                <div className="h-96" style={Img1Style}></div>
 
-            <div className="grid1-item items-center">
+                <div className="h-96" style={Img2Style}></div>
+
+       
+
                
             </div>
 
-        </div>
-        <Footer></Footer>
+       
         </div>
     )
 }
