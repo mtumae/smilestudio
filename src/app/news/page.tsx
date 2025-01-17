cf2afb78769ad8955721b9acbfa55f233638f135
mtume <mtume2016@gmail.com>
2025-01-17 12:36:36 +0300

    Adding content to news

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -5,6 +5,7 @@ import Footer from "../footer/page"
 import { Button } from "~/components/ui/button"
 import NavBar from "~/components/ui/navigation"
 import Autoplay from "embla-carousel-autoplay"
+import { MoveRight } from "lucide-react"
 
 import {
     Card,
@@ -40,19 +41,19 @@ import {
     justifyContent: 'center',
     alignItems: 'left',
     padding: '20px',
-    margin: '20px',
+
   };
 
 
   const Img2Style ={
-    backgroundImage: 'url(/sign.jpg)',
+    backgroundImage: 'url(/signblur.jpg)',
     backgroundSize: 'cover', 
     backgroundPosition: 'center',  
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'left',
     padding: '20px',
-    margin: '20px',
+
 
   }
 
@@ -62,13 +63,26 @@ export default function News(){
             <NavBar></NavBar>
         
             <div className="">
-                <div className="h-96" style={Img1Style}></div>
-                    <h1>News Blog</h1>
+                <div className="h-96 mt-7" style={Img1Style}>
+                <Card className=" p-5">
+                <CardHeader>
+                <CardTitle className="text-xl font-bold font-montserrat text-white">Implants: A Brief History</CardTitle>
+                <CardDescription className="text-white">Smile Studio Kenya</CardDescription>
+                </CardHeader>
+                <CardContent className=" text-white">
+                Most patients may not automatically associate the word “implants” with oral health. 
+                Most patients may also not know much about the history of dental implants, 
+                and the role they have played and continue to play in restorative dental health.
+                With over 95% success rates, dental implants are an excellent tooth replacement solution.
+                </CardContent>
+                <CardFooter className="text-ssblue hover:text-white cursor-pointer">
+                  read more <MoveRight className="ml-4 mt-1"/>
+                </CardFooter>
+              </Card> 
+                  
+                  </div> 
                 <div className="h-96" style={Img2Style}></div>
-               
             </div>
-
-       
         </div>
     )
 }
