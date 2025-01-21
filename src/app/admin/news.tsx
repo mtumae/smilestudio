d8b5537a102e3fc8a8cc6ca96e49e5c9da4d3921
mtume <mtume2016@gmail.com>
2025-01-21 12:30:55 +0300

    next font update said I do this

--- src/app/admin/news.tsx
+++ src/app/admin/news.tsx
@@ -5,9 +5,11 @@ import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
 
+
 import React from 'react'
 import { api } from "~/trpc/react";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import { title } from "process";
 
 const posts = {
     title : "",
@@ -20,16 +22,14 @@ export default function Blog(){
     const posts = api.post.getLatest.useQuery()
 
     return(
-        <div>
+        <>
             <h1 className="text-4xl font-montserrat mt-10 justify-self-center">Blog Posts</h1>
             <h1 className="justify-self-center">Create or edit a blog post for the news page</h1>
-
             <Tabs defaultValue="create" className="w-full">
                 <TabsList className="">
                     <TabsTrigger value="create">Create post</TabsTrigger>
                     <TabsTrigger value="edit">Edit post</TabsTrigger>
                 </TabsList>
-
                 <TabsContent value="create" className="grid grid-cols-1 w-full">
                 
                         <Label className="w-full">Main Title</Label>
@@ -43,13 +43,10 @@ export default function Blog(){
                         <Input className="h-60 mt-2 mb-2" placeholder="Enter Body..." ></Input>
                   
                         <Button className="w-1/3 bg-ssblue justify-self-end">Post</Button>
-                
                 </TabsContent>
-
                 <TabsContent value="edit">
                     <div className="grid grid-cols-1 m-10 gap-3">
-                       
-                            { posts.data?.map(( post ) =>
+                            {posts.data?.map(( post ) =>
                             <div className="">
                                 <Card key={post.id}>
                                     <CardHeader>
@@ -61,21 +58,15 @@ export default function Blog(){
                                     </CardContent>
                                     <CardFooter>
                                     <Button className="bg-ssblack text-ssblue mr-3">Edit<Edit/></Button> 
-                                    <Button className="bg-red text-white">Delete<Delete /></Button>
+                                    <Button variant={"destructive"} >Delete<Delete /></Button>
                                     </CardFooter>
                                 </Card>
-                                
                             </div>
                             )}
-                    
                     </div>
-                    
                 </TabsContent>
-            </Tabs>
-            
-           
-            
-        </div>
+            </Tabs>   
+        </>
     )
 }
 
